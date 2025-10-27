7ed74f67610c46424f42f6a8128f7a8f9afbceef
jaytomi <lawaljesutomisindeborah@gmail.com>
2025-10-27 17:38:25 +0000

    added some html code

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
     <title>Document</title>
   </head>
   <body>
-    
+    <form action="php_auth_system/register.php" method="post">
+        <label id="username"> Username </label><br><br>
+        <input type="text" class="username" placeholder="username..."> <br><br>
+        <label id="email"> E-mail </label><br><br>
+        <input type="email" class="email" placeholder="E-mail..."><br><br>
+        <label id="password"> Password </label><br><br>
+        <input type="password" class="password" placeholder="password....">
+</form>
+
   </body>
   </html>
